b56d706535957ae6c64bd7791d598f3420a4d722
Keerthana J <98206044+itzz-keerthi@users.noreply.github.com>
2022-11-17 07:44:33 +0530

    Vaccine Dates

---
+++ VDATES.java
@@ -0,0 +1,68 @@
+/* TASK
+Chef has taken his first dose of vaccine DD days ago. He may take the second dose no less than LL days and no more than RR days since his first dose.
+
+Determine if Chef is too early, too late, or in the correct range for taking his second dose.
+
+Input Format
+First line will contain TT, number of testcases. Then the testcases follow.
+Each testcase contains of a single line of input, three integers D, L, RD,L,R.
+Output Format
+For each test case, print a single line containing one string - "Too Early" (without quotes) if it's too early to take the vaccine, "Too Late" (without quotes) if it's too late to take the vaccine, "Take second dose now" (without quotes) if it's the correct time to take the vaccine.
+
+Constraints
+1 \leq T \leq 10^51≤T≤10 
+5
+ 
+1 \leq D \leq 10^91≤D≤10 
+9
+ 
+1 \leq L \leq R \leq 10^91≤L≤R≤10 
+9
+ 
+Subtasks
+Subtask 1 (100 points): Original constraints
+Sample 1:
+Input
+Output
+4
+10 8 12 
+14 2 10
+4444 5555 6666 
+8 8 12
+Take second dose now
+Too Late
+Too Early
+Take second dose now
+Explanation:
+Test case 11: The second dose needs to be taken within 88 to 1212 days and since the Day 1010 lies in this range, we can take the second dose now.
+
+Test case 22: The second dose needs to be taken within 22 to 1010 days since Day 1414 lies after this range, it is too late now.
+
+Test case 33: The second dose needs to be taken within 55555555 to 66666666 days and since the Day 44444444 lies prior to this range, it is too early now.  */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan = new Scanner(System.in);
+		int t=scan.nextInt();
+		for(int i=0;i<t;i++){
+		    int d=scan.nextInt();
+		    int l=scan.nextInt();
+		    int r=scan.nextInt();
+		    if(d>r){
+		        System.out.println("Too Late");
+		    }
+		    else if(d<l){
+		        System.out.println("Too Early");
+		    }
+		    else{
+		        System.out.println("Take second dose now");
+		    }
+		}
+	}
+}
