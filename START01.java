16a6eec814f5e6ebb8e1d023a4337617ad75e408
Keerthana J <98206044+itzz-keerthi@users.noreply.github.com>
2023-02-04 17:48:00 +0530

    Number Mirror

---
+++ START01.java
@@ -0,0 +1,48 @@
+/* PROBLEM
+
+Write a program that takes a number N as the input, and prints it to the output.
+
+Input Format
+The only line contains a single integer.
+
+Output Format
+Output the answer in a single line.
+
+Constraints
+0
+≤
+�
+≤
+1
+0
+5
+0≤N≤10 
+5
+ 
+Sample 1:
+Input
+Output
+123
+123
+Sample 2:
+Input
+Output
+15
+15                      */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println(n);
+	}
+}
